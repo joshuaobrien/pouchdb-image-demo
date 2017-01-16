fd0bb64428bda28376a0cc9d3f1f4488ea83c537
tig <joshujaT1@bitbucket.org>
2017-01-16 11:37:52 +1000

    revamp

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,12 +1,12 @@
 import PouchDB from 'pouchdb';
 import { Adapter } from 'ember-pouch';
 
-var remote = new PouchDB('http://localhost:5984:/squid');
+var remote = new PouchDB('http://localhost:5984/calamari');
 var db = new PouchDB('local_pouch');
 
 db.sync(remote, {
-  live: true,
-  retry: true
+   live: true,   // do a live, ongoing sync
+   retry: true   // retry if the connection is lost
 });
 
 export default Adapter.extend({
---
+++ app/controllers/photos.js
@@ -0,0 +1,51 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+
+
+    testaroo() {
+      alert('yes');
+
+      var pic = this.store.createRecord("picci", {
+          name: 'potatos'
+      });
+
+      pic.save();
+
+    },
+
+    uploadPhoto() {
+      var inputFile = document.querySelector('#inputFile');
+      var imageMetaData = document.querySelector('#img_meta_data');
+
+      var getFile = inputFile.files[0];
+
+      new PouchDB('lean').destroy().then(function () {
+        return new PouchDB('lean');
+      }).then(function (db) {
+        db.put({
+          _id: 'image',
+          _attachments: {
+            "file": {
+              type: getFile.type,
+              data: getFile
+            }
+          }
+        }).then(function() {
+          return db.getAttachment('image', 'file');
+        }).then(function(blob) {
+          var url = URL.createObjectURL(blob);
+          var img = document.createElement('img');
+          img.src = url;
+          document.body.appendChild(img);
+
+        }).catch(function (err) {
+          console.log(err);
+        })
+      });
+      }
+
+
+  }
+});
